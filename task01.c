e9c1356713c2c555b8aaf7838abc478ebfa43952
ervitis <vitomarti@gmail.com>
2014-03-11 13:32:59 +0100

    Added new files to the project

---
+++ task01.c
@@ -0,0 +1,39 @@
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/init.h>
+
+#define LICENSE_TYPE "GPL"
+#define AUTHOR "victor"
+#define DESCRIPTION "Task01, hello world"
+
+#define MESSAGE_HELLO "Hello World!"
+#define MESSAGE_CLEAN "Good bye, world"
+
+MODULE_LICENSE(LICENSE_TYPE);
+MODULE_AUTHOR(AUTHOR);
+MODULE_DESCRIPTION(DESCRIPTION);
+
+/*
+ * After boot the system, the kernel will call this module
+ */
+static int __init printHelloWorld(){
+  //for printing messages is better using printk than printf
+  //printk is used in kernel space and printf is used in user space
+  printk(KERN_INFO MESSAGE_HELLO);
+  return 0;
+}
+
+/*
+ * When cleaning the loadable modules is important to include
+ * the function __exit.
+ */
+static void __exit cleanHelloWorld(){
+  printk(KERN_INFO MESSAGE_CLEAN);
+}
+
+/*
+ * Call the modules
+ */
+module_init(printHelloWorld);
+module_exit(cleanHelloWorld);
+
